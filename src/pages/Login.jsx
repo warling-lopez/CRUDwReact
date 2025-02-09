0acfdd68d5d8a8450c89b25c4fbf717e3ef3739a
warling <warlinglopez01@gmail.com>
2025-02-09 16:36:43 -0400

    OAuth

---
+++ src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import {client} from '../supabase/client' 
+
+function Login() {
+  const [email, setEmail] = useState("")
+  const eventSubmit = async (e)=>{
+    e.preventDefault()
+    
+    try {
+      await client.auth.signInWithOtp({
+        email,
+        
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    
+
+  }
+  return (
+    <form onSubmit={eventSubmit}>
+      <input 
+      type="email" 
+      name="email" 
+      placeholder='youemail@site.com'
+      onChange={(e)=> setEmail(e.target.value)} />
+      <button>
+        Send
+      </button>
+    </form>
+  )
+}
+
+export default Login
